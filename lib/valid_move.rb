a6fc16cd0b43a962e64e0ae5df005aa6419b2164
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-22 23:46:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,15 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+the_board = ["",""," ", " ", "X", " ", " ", " ",""]
+
+def #position_taken?(board, index)
+
+(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+puts position_taken?(the_board, 0)
+puts position_taken?(the_board, 1)
+puts position_taken?(the_board, 2)
+puts position_taken?(the_board, 3)
+puts position_taken?(the_board, 4)
