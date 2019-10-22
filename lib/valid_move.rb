60e8cc0ef3b26f595952db2ab9497d5612406698
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-22 23:50:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,13 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 the_board = ["","X"," ", " ", " ", " ", " ", " "," "]
+puts "Enter a position on the board 1-9:"
+user_input = gets.chomp
+numbered_entered = user_input.to_i - 1
+
+def valid_move?(numbered_entered)
+  numbered_entered.between?(0,0) && !(position_taken?@the_board, numbered_entered))
+end
 
 def #position_taken?(board, index)
 
