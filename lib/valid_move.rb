1bcfe401a79a23d3616c973f7dc779f63787a6f7
clean-form-8028 <zamirahmo66@gmail.com>
2019-10-22 23:55:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,12 +12,25 @@ def valid_move?(numbered_entered)
 end
 
 def #position_taken?(board, index)
-
 (board[index] == " " || board[index] == "" || board[index] == nil)
 end
 
-puts position_taken?(the_board, 0)
-puts position_taken?(the_board, 1)
-puts position_taken?(the_board, 2)
-puts position_taken?(the_board, 3)
-puts position_taken?(the_board, 4)
+puts valid_move?(number_entered)
+
+puts #blank space
+puts #blank space
+
+the_nextboard = ["","X"," ", " ", " ", " ", " ", " "," "]
+puts "Enter a position on the board 1-9:"
+user_input = gets.chomp
+numbered_entered = user_input.to_i - 1
+
+def #valid_move?(numbered_entered)
+  numbered_entered.between?(0,8) && !(position_taken?(the_nextboard, numbered_entered))
+end
+
+def #position_taken?(board, index)
+(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+puts valid_move?(number_entered)
